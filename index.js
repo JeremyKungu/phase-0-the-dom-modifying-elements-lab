53624400e6cd5c38289645c599b7913eaa28e362
JeremyKungu <jeremikungu27@gmail.com>
2022-08-06 10:20:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.body.remove("#main");
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Jeremiah Kungu is the champion"
+document.body.append(newHeader);
+
+
+
+
+
